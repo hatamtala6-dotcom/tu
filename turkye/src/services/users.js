9801940a328b8efe6886947f1f842a19e66ef469
hatamtala6-dotcom <hatamtala6@gmail.com>
2026-06-11 01:28:20 +0300

    Update users.js

--- turkye/src/services/users.js
+++ turkye/src/services/users.js
@@ -1,1 +1,43 @@
-export async function registerUser(user, env) {}
+export async function registerUser(
+  telegramUser,
+  env
+) {
+  const id =
+    telegramUser.id.toString();
+
+  const exists =
+    await env.DB.prepare(
+      `
+      SELECT telegram_id
+      FROM users
+      WHERE telegram_id = ?
+    `
+    )
+      .bind(id)
+      .first();
+
+  if (exists) return;
+
+  await env.DB.prepare(
+    `
+    INSERT INTO users
+    (
+      telegram_id,
+      first_name,
+      username
+    )
+    VALUES
+    (
+      ?,
+      ?,
+      ?
+    )
+  `
+  )
+    .bind(
+      id,
+      telegramUser.first_name || "",
+      telegramUser.username || ""
+    )
+    .run();
+}
